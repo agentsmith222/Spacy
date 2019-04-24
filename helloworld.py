da8d77a9bbcdd32192ffb07d67751cf7af0c8b77
nathansire <nathantech2005@gmail.com>
2019-04-23 21:27:33 -0400

    parse a sentence

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,6 @@
 import spacy
 
 nlp = spacy.load("en_core_web_sm")
-doc = nlp(u"Apple is looking at buying U.K. startup for $1 billion")
+doc = nlp(u"Like you're someone trying to get free labor")
 for token in doc:
         print(token.text, token.pos_, token.dep_)
