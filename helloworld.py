e81c58e68dc4fd39189e79fde3def9f95f266c50
nathansire <nathantech2005@gmail.com>
2019-04-23 22:31:27 -0400

    simplify the example

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,11 @@
 import spacy
 
 nlp = spacy.load("en_core_web_sm")
-doc = nlp(u"Like you're someone trying to get free labor")
+doc = nlp(u"you are someone trying to get free labor")
 for token in doc:
+        print(token.text, token.pos_, token.dep_)
+
+
+doc2 = nlp(u"That is a really nice Github")
+for token in doc2:
         print(token.text, token.pos_, token.dep_)
